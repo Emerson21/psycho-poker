d2046d1683d8e2f569d70484d4e8fb75f91a2ae6
Emerson Alonso <alonso.emerson21@gmail.com>
2013-10-22 23:14:07 -0200

    criado e refatorado testes cases

--- src/test/java/br/com/psychopoker/MelhorMaoTest.java
+++ src/test/java/br/com/psychopoker/MelhorMaoTest.java
@@ -3,6 +3,8 @@ package br.com.psychopoker;
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.com.psychopoker.model.FourOfAKind;
+import br.com.psychopoker.model.FullHouse;
 import br.com.psychopoker.model.Monte;
 import br.com.psychopoker.model.StraightFlush;
 
@@ -10,10 +12,20 @@ public class MelhorMaoTest {
 	
 	@Test
 	public void combinacaoStraightFlush() throws Exception {
-		String entrada = "TH JH QC QD QS QH KH AH 2S 6S";
-		MelhorMao straightFlush = MelhorMao.resolver(new Monte(entrada)); 
-		
-		Assert.assertTrue(straightFlush instanceof StraightFlush);
+		Monte monte = new Monte("TH JH QC QD QS QH KH AH 2S 6S");
+		Assert.assertTrue(new StraightFlush(monte).matches());
+	}
+	
+	@Test
+	public void melhorMaoFourOfAKind() throws Exception {
+		Monte monte = new Monte("2H 2S 3H 3S 3C 2D 3D 6C 9C TH");
+		Assert.assertTrue(new FourOfAKind(monte).matches());
+	}
+	
+	@Test
+	public void fullHouse() throws Exception {
+		Monte monte = new Monte("2H 2S 3H 3S 3C 2D 9C 3D 6C TH");
+		Assert.assertTrue(new FullHouse(monte).matches());
 	}
 	
 }
