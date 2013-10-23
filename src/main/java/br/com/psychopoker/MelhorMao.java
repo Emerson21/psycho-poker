fb21c12b04a9f3a970b8ee45c06c4bf75e26d126
Emerson Alonso <alonso.emerson21@gmail.com>
2013-10-22 23:09:00 -0200

    transformando class em interface

--- src/main/java/br/com/psychopoker/MelhorMao.java
+++ src/main/java/br/com/psychopoker/MelhorMao.java
@@ -1,23 +1,8 @@
 package br.com.psychopoker;
 
-import br.com.psychopoker.model.Monte;
-import br.com.psychopoker.model.StraightFlush;
-import br.com.psychopoker.util.CollectionUtil;
 
-public class MelhorMao {
+public interface MelhorMao {
 
-	public static MelhorMao resolver(Monte monte) {
-		
-		if (new StraightFlush().matches(monte)) {
-			System.out.println("Mão: "+ CollectionUtil.join(monte.getCartasJogador(), ",") + " Monte: "
-					+ CollectionUtil.join(monte.getCartasMonte(), ",") + " Melhor Jogo: straight-flush (sequência numérica e de naipe)");
-			
-			return new StraightFlush();
-		}
-		
-		return null;
-
-	}
+	public boolean matches();
 	
-
 }
